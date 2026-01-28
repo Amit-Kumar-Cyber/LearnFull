eb0451d461b8ca891df41d55326866a14fd32f0f
Amit-Kumar-Cyber <asamiteng@gmail.com>
2026-01-29 02:11:28 +0530

    Authentication Signup Login

--- server/server.js
+++ server/server.js
@@ -10,18 +10,15 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('LearnFull API is running');
-});
+app.use('/api/auth', require('./routes/auth'));
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
